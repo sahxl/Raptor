b96a0ededae69fba3681cd893d34584a5dc4052d
jason <jason.zoller@outlook.com>
2022-08-13 09:20:24 +1000

    Files

---
+++ src/main/kotlin/cc/jasonn/raptor/RaptorSpigotPlugin.kt
@@ -0,0 +1,16 @@
+package cc.jasonn.raptor
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RaptorSpigotPlugin : JavaPlugin()
+{
+    override fun onEnable()
+    {
+
+    }
+
+    override fun onDisable()
+    {
+
+    }
+}
